c90adeea1bd5a73b6e9407f8d3f48d4bba664f15
Vincent Darwinkel <vincent.darwinkel@hotmail.nl>
2021-03-15 21:26:29 +0100

    Removed workflow

--- File_Service/GlobalSuppressions.cs
+++ File_Service/GlobalSuppressions.cs
@@ -6,3 +6,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Critical Code Smell", "S3871:Exception types should be \"public\"", Justification = "<Pending>", Scope = "type", Target = "~T:File_Service.CustomExceptions.UnprocessableException")]
+[assembly: SuppressMessage("Style", "IDE0090:Use 'new(...)'", Justification = "<Pending>", Scope = "member", Target = "~F:File_Service.HelperFiles.FilePaths.AllowedImagePaths")]
