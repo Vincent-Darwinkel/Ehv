3d86ba3fb64e222c0e21e726c3ff745c9c1023fe
Vincent Darwinkel <vincent.darwinkel@hotmail.nl>
2021-03-17 14:56:48 +0100

    Created docker compose and make ocelot work

--- Gateway_Service/Startup.cs
+++ Gateway_Service/Startup.cs
@@ -17,7 +17,7 @@ namespace Gateway_Service
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            var secret = "privatekey";
+            string secret = "privatekey";
             var key = Encoding.ASCII.GetBytes(secret);
             services.AddAuthentication(option =>
             {
